b955a5572c43007b2d837d0e3d1869c9ba475dfe
wjc <wangjincheng324@hotmail.com>
2015-07-17 16:47:15 +0800

    haha

--- Application/Admin/Controller/AuditController.class.php
+++ Application/Admin/Controller/AuditController.class.php
@@ -7,7 +7,11 @@ class AuditController extends Controller {
     	//dump($_SESSION);
     	$this->display();   
     }
-
+    public function fetchInnovatorUnderVerified(){
+        $Form = new Model();
+        $user = $Form->query("select * from entrepreneur_personal where reg_status = 0");
+        echo json_encode($user);
+    }
     public function projectList(){
 
     }
