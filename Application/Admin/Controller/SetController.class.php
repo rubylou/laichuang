275ab9681adbf97592a9c4bc3194a60d00f8a040
wjc <wangjincheng324@hotmail.com>
2015-08-19 11:07:58 +0800

    add home show edit

--- Application/Admin/Controller/SetController.class.php
+++ Application/Admin/Controller/SetController.class.php
@@ -25,4 +25,77 @@ class SetController extends Controller {
         $Form = new Model();
          $Form->execute('update admin_personal set admin_pwd="%s" where admin_id="%s"',$pwd,$id);
     }
+    public function homeShowProject(){
+        $Form = new Model();
+        $content=$Form->query("select * from home_show where tag='%d'",C(PROJECT_CODE));
+
+        $this->projects=json_encode( $content );
+
+        //dump($this->projects);
+        $this->display();
+    }
+    public function homeShowArticle(){
+        $Form = new Model();
+        $content=$Form->query("select * from home_show where tag='%d'",C(NEWS_CODE));
+
+        $this->news=json_encode( $content );
+
+        //dump($this->projects);
+        $this->display();
+    }
+
+    public function saveNews()
+    {
+        $ps=$_POST;
+        $Form = new Model();
+        
+        $Form->execute("delete from home_show where tag='%d'",C(NEWS_CODE));
+        if($_POST)
+        {
+          for($i=1;$i<=12;$i++)
+            {   
+               
+                $tmp=$ps['key'.$i];
+                
+                if($tmp){
+                    //dump($tmp."||||".$i);
+                    //$old[$i]=$tmp;
+                    $Form->execute("replace into home_show (content_id,tag) values ('%s','%d')",$tmp,C(NEWS_CODE));
+
+                }
+                    
+                
+            }  
+        }
+        
+        
+        echo 200;
+    }
+    public function saveProjects()
+    {
+        $ps=$_POST;
+        $Form = new Model();
+        
+        $Form->execute("delete from home_show where tag='%d'",C(PROJECT_CODE));
+        if($_POST)
+        {
+          for($i=1;$i<=12;$i++)
+            {   
+               
+                $tmp=$ps['key'.$i];
+                
+                if($tmp){
+                    //dump($tmp."||||".$i);
+                    //$old[$i]=$tmp;
+                    $Form->execute("replace into home_show (content_id,tag) values ('%s','%d')",$tmp,C(PROJECT_CODE));
+
+                }
+                    
+                
+            }  
+        }
+        
+        
+        echo 200;
+    }
 }
